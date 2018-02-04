7389749f7de8b7314880a813aae329d1b52e04b0
sunbaoxu <1194063234@qq.com>
2018-02-04 11:19:50 +0800

    待完善

---
+++ src/module/index/router/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+// const index           = resolve => require(['@/module/index/index.vue'], resolve);//首页
+
+
+Vue.use(Router);
+
+
+// let router = new Router({
+//   mode :'history',
+//   routes: [
+    
+//     {path: '/home',name: '首页',component: index,
+//       meta:{
+//         // title:'首页' // 标题设置在这里
+//       }
+//     }
+//   ]
+// })
